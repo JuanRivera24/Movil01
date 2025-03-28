460373e6c1fe207819c379d7440c0ece08a85c78
Brahian <angelbrahian026@gmail.com>
2025-03-28 18:49:06 -0500

    Cambio de libreria en locations

--- src/main/java/com/app/locations/domain/User.java
+++ src/main/java/com/app/locations/domain/User.java
@@ -1,6 +1,11 @@
-package com.app.users.domain;
+package com.app.locations.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
@@ -64,3 +69,4 @@ public class User {
         this.password = password;
     }
 }
+
