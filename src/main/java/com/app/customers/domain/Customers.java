c424a3d28a04bc2be65c97269e394619ebc579f5
Juan Rivera24 <juanmrivera0424@gmail.com>
2025-03-28 16:51:46 -0500

    a ver

--- src/main/java/com/app/customers/domain/Customers.java
+++ src/main/java/com/app/customers/domain/Customers.java
@@ -1,34 +1,43 @@
-package com.app.users.domain;
+package com.app.customers.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "Barberos")
+@Table(name = "Clientes")
 @Data
-public class User {
+public class Customers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String name;
+    private String nombre;
 
     @Column(nullable = false, unique = true)
-    private String email;
+    private String apellido;
 
     @Column(nullable = false)
-    private String password;
+    private String telefono;
+
+    @Column(nullable = false)
+    private String fechaNacimiento;
 
     // Empty constructor (required for JPA)
-    public User() {}
+    public Customers() {}
 
     // Constructor with parameters
-    public User(Long id, String name, String email, String password) {
+    public Customers(Long id, String nombre, String apellido, String telefono, String fechaNacimiento) {
         this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
     }
 
     // Getters y Setters
@@ -40,27 +49,35 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getNombre () {
+        return nombre;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
     }
 
-    public String getEmail() {
-        return email;
+    public String getApellido () {
+        return apellido;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setApellido (String apellido) {
+        this.apellido = apellido;
     }
 
-    public String getPassword() {
-        return this.password;
+    public String getTelefono () {
+        return this.telefono;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setTelefono (String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaNacimiento () {
+        return this.fechaNacimiento;
+    }
+
+    public void setFechaNacimiento (String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 }
